db20affa3ba4bed91653cc2e2e8ad63775dc4abd
Vishnu Mohan <vishnumohanmfc@gmail.com>
2024-04-11 09:42:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/user_and_admin");
+
+const express = require("express");
+const app = express();
+
+const nocache = require("nocache");
+app.use("/", nocache());
+const path = require("path");
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+//user route
+const user_route = require("./routes/userRoute");
+app.use("/", user_route);
+
+//admin route
+const admin_route = require("./routes/adminRoute");
+app.use("/admin", admin_route);
+
+app.listen(3000, () => {
+  console.log(`Server is running at http://localhost:3000`);
+});
